3a5e4a1eeee21e5f3ca7e32a53cda271b9aceb93
Marian.Borca <marian.borca@cossette.com>
2019-10-07 23:21:32 -0400

    Content dialog

--- src/app/dialogs/content-dialog/content-dialog.component.ts
+++ src/app/dialogs/content-dialog/content-dialog.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToolsService } from '../../services/tools.service';
 
 @Component({
   selector: 'app-content-dialog',
   templateUrl: './content-dialog.component.html',
-  styleUrls: ['./content-dialog.component.scss']
+  styleUrls: ['../dialog.scss', './content-dialog.component.scss']
 })
 export class ContentDialogComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public tools: ToolsService) { }
 
   ngOnInit() {
   }
